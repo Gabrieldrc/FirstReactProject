5997db1406fe5053aa914a8347c868ac50c8644b
Gabriel <you@example.com>
2020-07-06 15:43:47 -0300

    Correcting the Yelp json response

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -14,18 +14,19 @@ const Yelp = {
             return response.json();
         }).then(jsonResponse => {
             if (jsonResponse.businesses) {
+                console.log(jsonResponse.businesses);
                 return jsonResponse.businesses.map(business => {
                     return {
                         id: business.id,
-                        imageSrc: business.imageSrc,
+                        imageSrc: business.image_url,
                         name: business.name,
-                        address: business.address,
-                        city: business.city,
-                        state: business.state,
-                        zipCode: business.zipCode,
-                        category: business.category,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
                         rating: business.rating,
-                        reviewCount:business.reviewCount 
+                        reviewCount:business.review_count 
                     };
                 });
             }
